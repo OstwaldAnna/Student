de8b86597761bd532633c46ead00c4e6b7718737
OstwaldAnna <asyakynz@gmail.com>
2022-02-11 21:37:11 +0300

    .

---
+++ WpfApp1/Bd/DataHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using WpfApp1.Bd;
+
+namespace WpfApp1.Bd
+{//Класс помощник
+    //public
+    //public static
+    public class DataHelper
+    {
+        //Задача переменных, к БД и переходу страниц
+        public static StudSolidEntities entities;
+        public static Frame frame;
+    }
+}
